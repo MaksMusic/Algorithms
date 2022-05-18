18065ee10946176c90028e201f97585f3cadf528
MaksMusic <Maksmusic05@yandex.ru>
2022-05-18 08:07:52 +0300

    add algorithms array of numbers

--- src/main/kotlin/arrayOfNumber/ArrayOfNumberKt.kt
+++ src/main/kotlin/arrayOfNumber/ArrayOfNumberKt.kt
@@ -1,5 +1,20 @@
 package arrayOfNumber
 
+import java.util.*
+
 fun main() {
-    
+    var age = getArray(1997);
+    println(age[0])
+    println(age[1])
+    println(age[2])
+    println(age[3])
+}
+
+fun getArray(n:Long): LongArray {
+    var array = LongArray(n.toString().count())
+    for (i in 0..n.toString().count()-1){
+        array[i] =  n.toString()[i].toString().toLong()
+
+    }
+    return array
 }
