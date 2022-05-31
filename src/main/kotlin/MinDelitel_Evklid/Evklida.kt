b8fea43771ddee66af92e8583986ed8bf01364f9
MaksMusic <Maksmusic05@yandex.ru>
2022-05-31 15:03:49 +0300

    add algorithms MinValue and MaxValue

---
+++ src/main/kotlin/MinDelitel_Evklid/Evklida.kt
@@ -0,0 +1,16 @@
+package MinDelitel_Evklid
+
+
+fun main() {
+    print(minDelitel(695,1112))
+}
+   fun minDelitel ( a:Int,b:Int) :Int {
+       var nomer1 = a
+       var nomer2 = b
+        while (nomer2 !=0) {
+            var tmp = nomer1%nomer2
+            nomer1 = nomer2
+            nomer2 = tmp;
+        }
+        return nomer1;
+    }
