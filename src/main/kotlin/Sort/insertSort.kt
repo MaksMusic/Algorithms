1db094bf3fabb8c5c73d32e6654450934891332f
MaksMusic <Maksmusic05@yandex.ru>
2022-08-21 00:05:06 +0300

    add  insertSort

--- src/main/kotlin/Sort/insertSort.kt
+++ src/main/kotlin/Sort/insertSort.kt
@@ -1,16 +1,18 @@
 package Sort
 
 fun main() {
-    var arr = intArrayOf(3,54,1,4,2,34,21,4,12,5,6,75,4,3,22,4,5,11,7,8,9)
+    var arr = intArrayOf(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,11,7,20)
     insertSort(arr).forEach { print("$it ") }
 }
 
+//сортировка вставкой
 fun insertSort(intArray: IntArray) : IntArray{
     for (i in 1..intArray.lastIndex) {
         var element = intArray[i]
         var indexOne = i-1
 
         while(indexOne >= 0 && element<intArray[indexOne]){
+            intArray[indexOne+1] = intArray[indexOne]
             indexOne --
         }
         intArray[indexOne+1] = element
