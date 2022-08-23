5a09bd6044df14dd70073685d80f4b6fccc666a3
MaksMusic <Maksmusic05@yandex.ru>
2022-08-24 01:43:28 +0300

    add insertSort

--- src/main/kotlin/Sort/quickSort.kt
+++ src/main/kotlin/Sort/quickSort.kt
@@ -1,2 +1,46 @@
 package Sort
 
+fun main() {
+    var arr = intArrayOf(1,2,3,4,5)
+
+    quickSort(arr,0,arr.size-1).forEach { print("$it ") }
+}
+
+fun quickSort(intArray: IntArray,startIndex:Int,endIndex:Int) :IntArray {
+
+    if (startIndex >= endIndex) return intArray
+
+    var base  = intArray[startIndex]
+    var i = startIndex+1
+    var j = endIndex
+
+        while (i<=j){
+            println("for 1 ")
+
+            while (intArray[i] <= base && i<=j){
+                i++
+                println("while 1 ")
+            }
+
+            while (intArray[j] > base && i>=j){
+                j--
+                println("while 2 ")
+            }
+            if (i<j){
+                var temp = intArray[i]
+                intArray[i] = intArray[j]
+                intArray[j]=temp
+            }
+        }
+    if (intArray[startIndex]>intArray[j]){
+        var temp = intArray[startIndex]
+        intArray[startIndex] = intArray[j]
+        intArray[j] = temp
+    }
+
+    quickSort(intArray,startIndex,j-1)
+    quickSort(intArray,j+1,endIndex)
+
+    return intArray
+
+}
