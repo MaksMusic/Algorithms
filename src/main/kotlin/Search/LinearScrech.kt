6f51af2278e4a7efbbbc43c6c4eb98bb58c5bfb7
MaksMusic <Maksmusic05@yandex.ru>
2022-08-25 01:41:14 +0300

    add linearSearch

--- src/main/kotlin/Search/LinearScrech.kt
+++ src/main/kotlin/Search/LinearScrech.kt
@@ -1,15 +1,13 @@
 package Search
 
 fun main() {
-    var arr = intArrayOf(12,32,33,55,665,34,23,2,1,77,343,77)
-    println(linearSearch(arr,77))
+    var arr = intArrayOf(12, 32, 33, 55, 665, 34, 23, 2, 1, 77, 343, 77)
+    println(linearSearch(arr, 77))
 }
 
-fun linearSearch(intArray: IntArray,n:Int):Int{
+fun linearSearch(intArray: IntArray, n: Int): Int {
     for (i in intArray.indices) {
-        if (intArray[i]==n){
-            return i
-        }
+        if (intArray[i] == n) return i
     }
     return -1
 }
