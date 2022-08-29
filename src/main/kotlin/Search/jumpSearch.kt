3c7ab14db6dd71cd96223027217b81676c3b4856
MaksMusic <Maksmusic05@yandex.ru>
2022-08-29 14:01:53 +0300

    edit jump Search

--- src/main/kotlin/Search/jumpSearch.kt
+++ src/main/kotlin/Search/jumpSearch.kt
@@ -1,5 +1,36 @@
 package Search
 
+import java.util.*
+
 fun main() {
 
+    var arr = intArrayOf(21, 44, 15, 22, 34, 12, 11, 31, 14, 5, 36, 4, 13, 12, 17, 77, 5, 34)
+    arr.sort()
+    println(Arrays.toString(arr))
+    println(jumpSearch(arr,15))
+}
+
+
+fun jumpSearch(array: IntArray, n: Int): Int {
+    var arraySize = array.size
+    var jumpStep = Math.sqrt(array.size.toDouble()).toInt()
+    var previousStep = 0
+
+    while (array[Math.min(arraySize, jumpStep) - 1] < n) {
+        previousStep = jumpStep
+        jumpStep += Math.sqrt(arraySize.toDouble()).toInt()
+
+        if (previousStep >= arraySize)
+            return -1
+    }
+    while (array[previousStep] < n) {
+        previousStep++
+        if (previousStep == Math.min(jumpStep, arraySize))
+            return -1
+
+    }
+    if (array[previousStep] == n)
+        return previousStep
+
+    return -1
 }
